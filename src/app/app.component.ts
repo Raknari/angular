e3bd1be8ee89f540c1ca71760523af4744defacf
Gustavo Lopes <gustavotorres139@gmail.com>
2022-05-08 14:57:52 -0300

    Classes e Estilos Ng..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = "Melissa";
+  userData = {
+    email: "melissa@gmail.com",
+    role: 'Admin'
+  }
+
   title = 'angular';
 }
